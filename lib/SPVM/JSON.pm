489ec5796e9b32d9202e202ac9f25add2ac23f5b
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-25 10:21:59 +0900

    Fix old document

--- lib/SPVM/JSON.pm
+++ lib/SPVM/JSON.pm
@@ -9,8 +9,26 @@ our $VERSION = '0.01';
 SPVM::JSON - JSON serializing/deserializing
 
 =head1 SYNOPSYS
-  
-  use SPVM::JSON;
+
+B<SPVM:>
+
+  use JSON;
+
+  # new
+  my $json = JSON->new;
+
+  # decode
+  $json->decode($json_text);
+
+  # set the canonical flag on
+  $json->set_canonical(1);
+
+  # encode
+  my $encoded_json = $json->encode($spvm_data);
+
+B<Perl:>
+
+  use SPVM 'JSON';
 
   # new
   my $json = SPVM::JSON->new;
@@ -26,13 +44,17 @@ SPVM::JSON - JSON serializing/deserializing
 
 =head1 DESCRIPTION
 
-L<SPVM::JSON> converts SPVM data structures to JSON and vice versa.
+B<SPVM::JSON> converts SPVM data structures to JSON and vice versa.
+
+B<SPVM::JSON> is a L<SPVM> module.
+
+B<SPVM is yet before 1.0 released. SPVM is changed without warnings. There will be quite a lot of changes.>
 
 =head1 CLASS METHODS
 
 =head2 new
 
-  sub new : SPVM::JSON ()
+  static method new : SPVM::JSON ()
 
 Create new L<SPVM::JSON> object that can be used to de/encode JSON strings.
 
@@ -40,7 +62,7 @@ Create new L<SPVM::JSON> object that can be used to de/encode JSON strings.
 
 =head2 encode
 
-  sub encode : string ($self : self, $object : object)
+  method encode : string ($object : object)
 
 Converts the given SPVM data structure (undef or a object of numeric,
 L<string>, L<SPVM::JSON::Bool>, L<SPVM::Hash> or L<SPVM::ObjectList>)
@@ -48,7 +70,7 @@ to its JSON representation.
 
 =head2 decode
 
-  sub decode : object ($self : self, $json : string)
+  method decode : object ($json : string)
 
 The opposite of encode: expects a JSON text and tries to parse it, returning
 the resulting object. Dies on error. Numbers in a JSON text are converted
@@ -56,13 +78,13 @@ to L<SPVM::Double>.
 
 =head2 set_canonical
 
-  sub set_canonical : void ($self : self, $enable : byte)
+  method set_canonical : void ($enable : byte)
 
 If C<$enable> is true, then the encode method will output JSON objects by
 sorting their keys. This is adding a comparatively high overhead.
 
 =head2 canonical
 
-  sub canonical : byte ($self : self)
+  method canonical : byte ()
 
 Get the canonical flag.
